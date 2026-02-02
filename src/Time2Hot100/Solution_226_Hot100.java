149cb7bf51301aa10e89464b14bc3ccfc8d6a4cb
FKBGRENSHE <fkbgrenshe@163.com>
2026-02-02 18:45:52 +0800

    第二遍力扣Hot100

--- src/Time2Hot100/Solution_226_Hot100.java
+++ src/Time2Hot100/Solution_226_Hot100.java
@@ -1,9 +1,40 @@
 package Time2Hot100;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @Author: Bingyu Chen
  * @CreateTime: 2026-02-02
  * @Description:
  */
 public class Solution_226_Hot100 {
+
+    Deque<TreeNode> deque = new ArrayDeque<>();
+
+    TreeNode temp = null;
+
+    public TreeNode invertTree(TreeNode root) {
+        doReverse(root);
+
+        return root;
+    }
+
+    private void doReverse(TreeNode curNode) {
+        if (curNode == null){
+            return;
+        }
+
+        // 左
+        doReverse(curNode.left);
+        // 右
+        doReverse(curNode.right);
+        // 中
+        temp = curNode.left;
+        curNode.left = curNode.right;
+        curNode.right = temp;
+
+
+    }
 }
