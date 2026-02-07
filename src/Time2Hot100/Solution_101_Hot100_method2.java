f53234d450826c2296bf5a8f53664da500725c5f
FKBGRENSHE <fkbgrenshe@163.com>
2026-02-07 14:46:55 +0800

    第二遍力扣Hot100

--- src/Time2Hot100/Solution_101_Hot100_method2.java
+++ src/Time2Hot100/Solution_101_Hot100_method2.java
@@ -6,4 +6,29 @@ package Time2Hot100;
  * @Description: 对称二叉树的递归判断
  */
 public class Solution_101_Hot100_method2 {
+
+
+    public boolean isSymmetric(TreeNode root) {
+        return doCheck(root.left, root.right);
+    }
+
+    private boolean doCheck(TreeNode left, TreeNode right) {
+
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+
+        return left.val == right.val && doCheck(left.left, right.right) && doCheck(left.right, right.left);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+//        root.left.left = new TreeNode(3);
+        root.right.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+//        root.right.left = new TreeNode(4);
+        Solution_101_Hot100_method2 solution101Hot100 = new Solution_101_Hot100_method2();
+        System.out.println(solution101Hot100.isSymmetric(root));
+    }
 }
--- src/Time2Hot100/Solution_543_Hot100.java
+++ src/Time2Hot100/Solution_543_Hot100.java
@@ -1,12 +1,48 @@
-//package Time2Hot100;
-//
-///**
-// * @Author: Bingyu Chen
-// * @CreateTime: 2026-02-02
-// * @Description:
-// */
-//public class Solution_543_Hot100 {
-//    public int diameterOfBinaryTree(TreeNode root) {
-//
-//    }
-//}
+package Time2Hot100;
+
+/**
+ * @Author: Bingyu Chen
+ * @CreateTime: 2026-02-02
+ * @Description: 543 二叉树直径
+ */
+public class Solution_543_Hot100 {
+
+    int maxlength = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+
+        int i = doSearch(root);
+
+        return maxlength;
+    }
+
+    public int doSearch(TreeNode curNode){
+        if (curNode == null){
+            return 0;
+        }
+
+        int leftMaxDepth = doSearch(curNode.left);
+        int rightMaxDepth = doSearch(curNode.right);
+
+        maxlength = Math.max(maxlength, leftMaxDepth+rightMaxDepth);
+
+        return Math.max(leftMaxDepth,rightMaxDepth)+1;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        Solution_543_Hot100 solution543Hot100 = new Solution_543_Hot100();
+        System.out.println(solution543Hot100.diameterOfBinaryTree(root));
+
+    }
+
+
+
+
+}
