2de8ab8721857023d2e2be6e922fa574d25e6623
FKBGRENSHE <fkbgrenshe@163.com>
2026-02-13 19:44:04 +0800

    第二遍力扣Hot100

---
+++ src/ACM/InputAndOutput.java
@@ -0,0 +1,9 @@
+package ACM;
+
+/**
+ * @Author: Bingyu Chen
+ * @CreateTime: 2026-02-13
+ * @Description: ACM模式下的输入输出
+ */
+public class InputAndOutput {
+}
---
+++ src/luogu/P2871.java
@@ -0,0 +1,21 @@
+package luogu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @Author: Bingyu Chen
+ * @CreateTime: 2026-02-13
+ * @Description: 01背包问题
+ */
+public class P2871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+//        int n =
+
+    }
+}
