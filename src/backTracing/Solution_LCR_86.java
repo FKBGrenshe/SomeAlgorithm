bc1e5fb487e2e96484af4764ce7c552c9c8051e8
FKBGRENSHE <fkbgrenshe@163.com>
2026-03-03 20:23:14 +0800

    回溯复习

---
+++ src/backTracing/Solution_LCR_86.java
@@ -0,0 +1,108 @@
+package backTracing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @Author: Bingyu Chen
+ * @CreateTime: 2026-03-03
+ * @Description: 分割回文串
+ */
+public class Solution_LCR_86 {
+
+    List<List<String>> globalResult = new ArrayList<>();
+    List<String> curResult = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
+    String s;
+
+
+    /*public String[][] partition(String s) {
+        this.s = s;
+        backTracing("",s);
+
+        String[][] finalResult = new String[globalResult.size()][];
+        for (int i = 0; i < globalResult.size(); i++) {
+
+            List<String> strings = globalResult.get(i);
+            strings.removeFirst();
+            finalResult[i] = new String[strings.size()];
+
+            for (int j = 0; j < finalResult[i].length; j++) {
+                finalResult[i][j] = strings.get(j);
+            }
+        }
+
+        return finalResult;
+    }*/
+    public String[][] partition(String s) {
+        this.s = s;
+        backTracing(null,s);
+
+        globalResult.forEach(List::removeFirst);
+
+        String[][] finalResult = new String[globalResult.size()][];
+        for (int i = 0; i < globalResult.size(); i++) {
+
+            List<String> strings = globalResult.get(i);
+            finalResult[i] = new String[strings.size()];
+
+            for (int j = 0; j < finalResult[i].length; j++) {
+                finalResult[i][j] = strings.get(j);
+            }
+        }
+
+        return finalResult;
+    }
+
+    void backTracing(String curString, String resString){
+        if (!isHuiWen(curString)){
+            return;
+        }
+
+
+        curResult.add(curString);
+
+        if (resString.isEmpty()){
+            globalResult.add(new ArrayList<>(curResult));
+//            curResult.remove(curString);
+            curResult.removeLast();
+            return;
+        }
+
+        for (int i = 0; i < resString.length(); i++) {
+            String nextSubString = resString.substring(0,i+1);
+            String nextResString = resString.substring(i+1,resString.length());
+            backTracing(nextSubString,nextResString);
+        }
+//        curResult.remove(curString);
+        curResult.removeLast();
+    }
+
+    boolean isHuiWen(String s){
+        if (Objects.isNull(s)){
+            return true;
+        }
+        // todo
+        int beginIdx = 0, endIdx = s.length()-1;
+        while (beginIdx <= endIdx){
+            if (s.charAt(beginIdx++) != s.charAt(endIdx--)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Solution_LCR_86 solutionLcr86 = new Solution_LCR_86();
+//        System.out.println(solutionLcr86.isHuiWen(""));
+
+//        String[][] aabs = solutionLcr86.partition("cbbbcc");
+//        List<List<String>> cbbbcc = solutionLcr86.partition("aabba");
+        String[][] cbbbcc = solutionLcr86.partition("cbbbcc");
+
+
+    }
+}
